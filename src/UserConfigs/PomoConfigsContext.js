53de9fccd82dfa72b54898f0092d082ae6783e92
chenditc <chenditc@umich.edu>
2022-12-21 10:14:51 +0800

    Update default config

--- src/UserConfigs/PomoConfigsContext.js
+++ src/UserConfigs/PomoConfigsContext.js
@@ -5,7 +5,7 @@ const defaultPomoConfigs = {
   alertRestSeconds: 5 * 60,
   enableNotification: false,
   notificationIntervalSeconds: 60,
-  notificationIntervalMultiplier: 1,
+  notificationIntervalMultiplier: 1.5,
   tempMissingSeconds: 60,
   history: {
     maxLocalStorageTimeSlot: 100,
@@ -14,7 +14,7 @@ const defaultPomoConfigs = {
   },
   faceRecognition: {
     minDetectionInterval: 5,
-    maxDetectionInterval: 30,
+    maxDetectionInterval: 40,
     scoreThreshold: 0.95,
     showFaceRecognitionStatus: false,
     showFaceRecognitionCanvas: false,
@@ -22,7 +22,7 @@ const defaultPomoConfigs = {
   },
   enableDetection: false,
   enablePlayer: false,
-  playerUrl: "https://www.youtube.com/watch?v=q4YyeEM9jsc",
+  playerUrl: "https://www.youtube.com/watch?v=tfBVp0Zi2iE",
   focusNotificationText: "Time to focus!",
   restNotificationText: "Let's take a break."
 }
